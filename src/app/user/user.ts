af0a4430956ce726be5516492c4a8acdc9c20b59
Adem <ado.designs@outlook.de>
2025-11-07 19:41:44 +0100

    feat: new users from firebase too table in user c

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -3,6 +3,7 @@ import { DialogAddUser } from '../dialog-add-user/dialog-add-user';
 import { MATERIAL_IMPORTS } from '../shared/materials';
 import { MatDialog } from '@angular/material/dialog';
 import { UserClass } from '../models/user.class';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-user',
@@ -11,10 +12,16 @@ import { UserClass } from '../models/user.class';
   styleUrl: './user.scss',
 })
 export class User {
+  allUsers: any[] = [];
   user = new UserClass();
-  constructor(public dialog: MatDialog) {}
+  constructor(private firebaseService: FirebaseService, public dialog: MatDialog) {
+      this.firebaseService.getUsers().subscribe((changes) => {
+      this.allUsers = changes;
+      console.log("Users from Firestore", changes);
+    });
+  }
 
-openDialog() {
-  this.dialog.open(DialogAddUser);
-}
+  openDialog() {
+    this.dialog.open(DialogAddUser);
+  }
 }
