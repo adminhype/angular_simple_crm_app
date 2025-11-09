ebbfc2a4aa2b16c84564ab8d8d83824fc4d2f07d
Adem <ado.designs@outlook.de>
2025-11-09 13:39:58 +0100

    feat: user detail header and card base structure

--- src/app/user-detail/user-detail.ts
+++ src/app/user-detail/user-detail.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MATERIAL_IMPORTS } from '../shared/materials';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  imports: [MATERIAL_IMPORTS],
   templateUrl: './user-detail.html',
   styleUrl: './user-detail.scss',
 })
